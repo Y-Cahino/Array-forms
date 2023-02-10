558bdbc8ba0dd24c5dbec997e1f77ca9128c21b2
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-02-10 12:47:37 +0100

    Aggiungere i file di progetto.

---
+++ Array-forms/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Array_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(567, 30);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(201, 382);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Array-forms/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array_forms
+{
+    public partial class Form1 : Form
+    {
+        int []a=new int[10];
+        public Form1()
+        {
+            InitializeComponent();
+            CreateMyMainMenu();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void CreateMyMainMenu()
+        {
+            // Creazione menù vuoto
+            MainMenu mainMenu1 = new MainMenu();
+            //categorie menù
+            MenuItem menu1 = new MenuItem();
+            MenuItem menu2= new MenuItem();
+            MenuItem menu3 = new MenuItem();
+            MenuItem menu4 = new MenuItem();
+
+            menu1.Text = "Inserisci Elemento";
+            menu2.Text = "Calcola somma";
+            menu3.Text = "Visualizza Array";
+            menu4.Text = "Cancellazione";
+            menu2.Text = "Modifica Array";
+            // Questo porta le scritte in alto
+            mainMenu1.MenuItems.Add(menu1);
+            mainMenu1.MenuItems.Add(menu2);
+            mainMenu1.MenuItems.Add(menu3);
+            mainMenu1.MenuItems.Add(menu4);
+
+            // Questo definisce quale tra i main è il menù
+            Menu = mainMenu1;
+        }
+        
+    }
+}
